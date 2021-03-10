64e5849552b0b8d37e43e876afa4e12d76a92611
Hendro Wijaya <wijayahendro@gmail.com>
2021-03-10 12:00:46 +0700

    postgres database uri

--- aplikasi/config.py
+++ aplikasi/config.py
@@ -3,6 +3,6 @@ from datetime import timedelta
 
 SECRET_KEY = '-9UDAJR2Gldkja0fu397hhhhkj;dlaf'
 #SQLALCHEMY_DATABASE_URI = 'sqlite:///sample.db'
-SQLALCHEMY_DATABASE_URI = 'postgresql://hendro:echo2019@localhost/hendro'
+SQLALCHEMY_DATABASE_URI = 'postgresql://hendro:echo123@localhost/hendro'
 SQLALCHEMY_TRACK_MODIFICATIONS = False
 PERMANENT_SESSION_LIFETIME =  timedelta(minutes=10)
