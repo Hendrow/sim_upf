1b90ee354cf5c907192987523a36a69357d908d4
Hendrow <wijayahendro@gmail.com>
2021-09-28 08:58:14 +0700

    perbaikan penulisan field petugas ke petugas_catat

--- aplikasi/pinjam/forms.py
+++ aplikasi/pinjam/forms.py
@@ -10,12 +10,12 @@ def pilih_fasyankes():
     return Fasyankes.query
 
 class Input(FlaskForm):
-    petugas = SelectField('Petugas Pencatat',
+    petugas_catat = SelectField('Petugas Pencatat',
         choices=[('none','...Pilih..'),('Andi P','Andi Purba'),('Azizil T','Azizil Tiara Putra'),('M Agung','M Agung S'),('M Ilham','M Ilham Maulana'),('Sandra M','Sandra Monika'),('Demtania','Demtania'),('Robiyansyah','Robiyansyah'),('Elvira','Elvira')])
-    peminjam =  SelectField('Peminjam Alat',
+    peminjam_alat =  SelectField('Peminjam Alat',
         choices=[('none','...Pilih..'),('Andi P','Andi Purba'),('Azizil T','Azizil Tiara Putra'),('M Agung','M Agung S'),('M Ilham','M Ilham Maulana'),('Sandra M','Sandra Monika'),('Demtania','Demtania'),('Robiyansyah','Robiyansyah'),('Elvira','Elvira')])    
     tanggal = DateField('Tanggal peminjaman', validators=[DataRequired()])
-    kordinatorTim = SelectField('Kordinator Tim',
+    kordinator_tim = SelectField('Kordinator Tim',
         choices=[('none','...Pilih..'),('Andi P','Andi Purba'),('Azizil T','Azizil Tiara Putra'),('M Agung','M Agung S'),('M Ilham','M Ilham Maulana'),('Sandra M','Sandra Monika'),('Demtania','Demtania'),('Robiyansyah','Robiyansyah'),('Elvira','Elvira')])
     fasyankes = QuerySelectField(query_factory=pilih_fasyankes, allow_blank=True, get_label='nama')
     simpan = SubmitField('Lanjut')
