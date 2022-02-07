d68537a012abe18e89da473725cc3239ee25e0d1
Hendrow <wijayahendro@gmail.com>
2022-02-07 09:57:10 +0700

    uri sqlite

--- aplikasi/config.py
+++ aplikasi/config.py
@@ -2,8 +2,8 @@ from datetime import timedelta
 
 
 SECRET_KEY = '-9UDAJR2Gldkja0fu397hhhhkj;dlaf'
-# SQLALCHEMY_DATABASE_URI = 'sqlite:///db.sqlite3'
+SQLALCHEMY_DATABASE_URI = 'sqlite:///db.sqlite3'
 # SQLALCHEMY_DATABASE_URI = 'postgresql://upfk:mariana01@localhost/upfk'
-SQLALCHEMY_DATABASE_URI = 'postgresql://postgres:echo1234@localhost/upfk'
+# SQLALCHEMY_DATABASE_URI = 'postgresql://postgres:echo1234@localhost/upfk'
 SQLALCHEMY_TRACK_MODIFICATIONS = False
 PERMANENT_SESSION_LIFETIME =  timedelta(minutes=10)
