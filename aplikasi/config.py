e349ee30e03b26248a1af87c3f64f5d7ad099aa6
Hendro Wijaya <wijayahendro@gmail.com>
2022-05-18 11:00:27 +0700

    session lifetime 30 menit

--- aplikasi/config.py
+++ aplikasi/config.py
@@ -6,4 +6,4 @@ SQLALCHEMY_DATABASE_URI = 'sqlite:///db.sqlite3'
 # SQLALCHEMY_DATABASE_URI = 'postgresql://upfk:mariana01@localhost/upfk'
 # SQLALCHEMY_DATABASE_URI = 'postgresql://postgres:echo1234@localhost/upfk'
 SQLALCHEMY_TRACK_MODIFICATIONS = False
-PERMANENT_SESSION_LIFETIME =  timedelta(minutes=10)
+PERMANENT_SESSION_LIFETIME =  timedelta(minutes=30)
